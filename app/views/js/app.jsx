9a845946f2f7a3950c242381cddb3ffa53e7f66c
Alexey Matsuhanov <manexin1@gmail.com>
2021-05-07 10:09:05 +0300

    add new line added new line to app.jsx

--- app/views/js/app.jsx
+++ app/views/js/app.jsx
@@ -6,6 +6,7 @@ class HelloWorld extends React.Component {
           <div className="col-xs-8 col-xs-offset-2 jumbotron text-center">
             <h2>Andersen DevOps course</h2>
             <h1>Hello EXAM+Kubernetes!</h1>
+	    <h3>CI/CD GitHub+GKE</h3>
             <h3>Hello World web app created with Golang + Gin + React</h3>
             <h4><a href="https://github.com/cskonopka/go-gin-react">Github</a></h4>
           </div>
